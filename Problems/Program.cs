9b108d70d714ce9139d15e0f2e84626b57558a68
Boitshoko <g17p9962@Campus.ru.ac.za>
2019-06-03 13:34:14 +0200

    Commit tests. solution and problems project

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,8 +2,20 @@
 
 namespace Problems
 {
-    class Program
+   public class Program
     {
+         string Prefix(string input)
+        {
+            if (input.Equals(null))
+            {
+                return null;
+            }
+            int length = input.Length;
+            string[] arrayOfWords = input.Split(' ');
+            int numOfWords = arrayOfWords.Length;
+            string output = $"{length},{numOfWords}: {input}";
+            return output;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
